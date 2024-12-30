fe62c2527af4cab7a00e4ce68e2cdc262495ee69
gabrielalberini <gabialberini733@gmail.com>
2024-12-30 13:12:25 -0300

    clase 11 - introducción a js

---
+++ clase-11-30-12-2024/conceptos/app.js
@@ -0,0 +1,85 @@
+// Variables
+// Espacio reservado en memoria
+
+// let button = document.querySelector("button")
+let color = "rojo"
+
+color = "azul"
+
+console.log("El color guardado es " + color)
+
+const PI = 3.1416
+
+console.log("El valor de PI siempre será " + PI)
+
+const nombre = "Gabriel"
+const apellido = "Alberini"
+const edad = 30
+const sexo = "H"
+
+console.log("Hola, soy" + " " + nombre + " " + apellido + " y tengo" + " " + edad + " " + "años.")
+
+// Var esta deprecada, no se usa más
+var gustoDeHelado = "banana con dulce de leche"
+
+// ---------------------------------------------
+
+// Tipos de datos
+
+// Palabras | string | Cadena de caracteres
+const animal = "perro"
+
+// Números | number
+let year = 30
+let temperatura = 32.4
+let saldo = -30
+
+// Lógicos | Boolean
+let lluvia = true
+let soleado = false
+let libreDeMultas = false
+
+// Indefinido | undefined
+let superpoder
+
+console.log(superpoder)
+
+// ---------------------------------------
+// Operadores de comparación
+// Siempre da un booleano como respuesta
+// Validar que la persona sea mayor de edad
+
+let edadPersona = 18
+let edadMinima = 18
+
+// Mayor que >
+// Mayor o igual que >=
+// Menor que <
+// Manor o igual que <=
+// Igual que ===
+// Diferente que !==
+
+// console.log(edadPersona >= edadMinima)
+
+let pass = "pepeelmascapo"
+let comparedPass = "pepeelmascapo"
+
+// console.log(pass === comparedPass)
+
+// -----------------------------
+// Operadores aritmeticos
+// + - * / %
+
+console.log("Suma:", 1 + 1)
+console.log("Resta:", 10 - 5)
+console.log("División:", 100 / 110)
+console.log("Multiplicación:", 10 * -10)
+console.log("Resto:", 16 % 2)
+
+// Concatenación
+// Unión de dos string
+
+const nombreMascota = "Aranadela"
+const apellidoMascota = "Alberini"
+
+console.log(nombreMascota + " " + apellidoMascota)
---
+++ clase-11-30-12-2024/mostrar-ocultar-div/app.js
@@ -0,0 +1,27 @@
+// Paso 1: Conectar el botón y el div
+
+let button = document.querySelector("button")
+let div = document.querySelector("div")
+
+// Mostrar lo almacenado en la consola
+// console.log(button, div)
+
+// Javascript manipula el DOM -> Document object model (HTML)
+
+// Paso 2: Detectar el click en el botón
+
+function analizarDiv() {
+  // Paso 3: Enseñarle a la pc que si el div tiene la clase "mostrar" se la elimine y le agregue "ocultar" y viceversa
+
+  if (div.classList.contains("mostrar")) {
+    div.classList.remove("mostrar")
+    div.classList.add("ocultar")
+    button.style.backgroundColor = "red"
+  } else {
+    div.classList.remove("ocultar")
+    div.classList.add("mostrar")
+    button.style.backgroundColor = "yellowgreen"
+  }
+}
+
+button.addEventListener("click", analizarDiv) 
